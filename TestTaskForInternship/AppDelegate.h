e7edf5d679dde90eaed5d39a593ddfa4a5a9d85a
Nikolay <nikolay@Nikolays-MacBook-Pro-2.local>
2017-01-22 18:11:29 +0300

    Initial Commit

---
+++ TestTaskForInternship/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTaskForInternship
+//
+//  Created by Nikolay on 22/01/2017.
+//  Copyright © 2017 GnomVelikan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestTaskForInternship/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTaskForInternship
+//
+//  Created by Nikolay on 22/01/2017.
+//  Copyright © 2017 GnomVelikan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
